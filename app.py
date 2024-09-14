f77f1bc6b7591dde0ce94691e0bb66e99e7a1898
Victorjaglen <victorjaglen2691998@gmail.com>
2024-09-14 13:16:14 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,5 @@
+from phrasehunter.game import Game    # Imported Game class
+
+if __name__ == '__main__':    # Check if the script is run directly
+    game = Game()    # Created a Game instance
+    game.start()    # Started the game
